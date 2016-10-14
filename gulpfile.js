bd91fda36b62fcb1e80e4f76801ec6dcbad131a0
Alexander Jacks <alexd.jacks@gmail.com>
2016-10-14 12:03:55 -0700

    tweaking Sass tasks to automate rebuilding

--- gulpfile.js
+++ gulpfile.js
@@ -101,6 +101,7 @@ gulp.task ("serve", ["build"], function() {
   gulp.watch (["js/*.js"], ["jsBuild"]);
   gulp.watch (["bower.json"], ["bowerBuild"]);
   gulp.watch (["*.html"], ["htmlBuild"]);
+  gulp.watch(["css/*.css"], ['cssBuild']);
   gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
