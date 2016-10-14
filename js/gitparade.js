ea2c38c0bbd1b79184535705ea1cfef5b2b1bdf0
Alexander Jacks <alexd.jacks@gmail.com>
2016-10-14 12:42:58 -0700

    adds link to JSON of followers for lookup result; needs formatting

--- js/gitparade.js
+++ js/gitparade.js
@@ -6,11 +6,12 @@ function GitParade(){
 
 GitParade.prototype.userFind = function(userName) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(userResult){
-    $('#output1').append(userResult.name);
-    $('#output2').append(userResult.login);
-    $('#output3').append(userResult.bio);
-    $('#output4').append(userResult.followers);
-    $('#output5').append(userResult.following);
+    var followersURL = userResult.followers_url;
+    $('#output1').text(userResult.name);
+    $('#output2').text(userResult.login);
+    $('#output3').text(userResult.bio);
+    $('#output4').append("<a href="+followersURL+">"+userResult.followers+"</a>");
+    $('#output5').text(userResult.following+"</a>");
   }).fail(function(error){
         console.log(error.responseJSON.message);
   });
