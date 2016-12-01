48a381ef85b1976731482dcf08d0e17b29503f5c
Alex Jacks <alexd.jacks@gmail.com>
2016-12-01 14:27:27 -0800

    sets up experimental page to fill w vimeos; covervid dependency loads via pipeline

--- js/gitparade.js
+++ js/gitparade.js
@@ -1,26 +1,26 @@
-// make API request
-var apiKey = require('./../.env').apiKey;
-
-function GitParade(){
-}
-
-GitParade.prototype.userFind = function(userName) {
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(userResult){
-    var followersURL = userResult.followers_url;
-    $('#output1').text(userResult.name);
-    $('#output2').text(userResult.login);
-    $('#output3').text(userResult.bio);
-    $('#output4').append("<a href="+followersURL+">"+userResult.followers+"</a>");
-    $('#output5').text(userResult.public_repos);
-      $.get('https://api.github.com/users/' +userName+ '/repos?&per_page=100&access_token=' + apiKey).then(function(reposResult){
-        for (var i=0; 1<100; i++){
-          $('#repoList').append("<h4>" +reposResult[i].name+ "</h4>");
-          $('#repoList').append("<h5>" +reposResult[i].description+ "</h5>");
-        }
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
-  });
-};
-
-exports.gitParadeModule = GitParade;
+// // make API request
+// // var apiKey = require('./../.env').apiKey;
+//
+// function GitParade(){
+// }
+//
+// GitParade.prototype.userFind = function(userName) {
+//   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(userResult){
+//     var followersURL = userResult.followers_url;
+//     $('#output1').text(userResult.name);
+//     $('#output2').text(userResult.login);
+//     $('#output3').text(userResult.bio);
+//     $('#output4').append("<a href="+followersURL+">"+userResult.followers+"</a>");
+//     $('#output5').text(userResult.public_repos);
+//       $.get('https://api.github.com/users/' +userName+ '/repos?&per_page=100&access_token=' + apiKey).then(function(reposResult){
+//         for (var i=0; 1<100; i++){
+//           $('#repoList').append("<h4>" +reposResult[i].name+ "</h4>");
+//           $('#repoList').append("<h5>" +reposResult[i].description+ "</h5>");
+//         }
+//       }).fail(function(error){
+//         console.log(error.responseJSON.message);
+//       });
+//   });
+// };
+//
+// exports.gitParadeModule = GitParade;
--- readme.md
+++ readme.md
@@ -1,34 +1,25 @@
-# Gitparade :octocat::loop::thought_balloon:
-## _Github API demonstration_
-_an Epicodus Project, October 2016_
+# :roller_coaster::video_camera: Lottavid
+## _JQuery plugin 'covervid' demo_
+
 ### By _Alexander Jacks_
 
 ### Description
-Gitparade allows a user to enter the name of a Github member and see their recent workflow (repositories, repos) with descriptions. Only publicly-shared work is available.
+Responsive grid of free videos from Vimeo, for templating.
 
 ## Setup
 1. Clone/download this project directory
 
-2. Request your own API Key from Github Settings [as seen here](https://dl.dropboxusercontent.com/s/qk0qcsy487yg7ci/githubgeneratetoken.png?dl=0)
-
-3. Create a one-line file named ".env" in your project directory to include your key.
+2. Open Terminal in your project directory
 ```
-exports.apiKey = "your-github-api-key";
-```
-
-4. Open terminal in your project directory
-```
-$ npm install
-$ bower install
-$ gulp serve
+$ npm i && bower i && gulp serve
 ```
 
 ## Known Issues
-Descriptions are sourced directly from Github README files like this one. As GH users supply their own repository descriptions, the quality and detail of these will vary. Caveat utilitor!
+Totes a work in progress.
 
 
 ## Technologies Used
-- Development: Node.js, JQuery
+- Development: Node.js, JQuery + covervid
 - Configuration: npm, Gulp, Bower
 - Design: Bootstrap 3, Sass
 - Testing: JSHint, Chrome
